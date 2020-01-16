36ce39a96d64b133438bbf649384b99fb0646135
Felipe Lozada <felipe@penca.com.mx>
2020-01-16 15:58:39 -0600

    blog

--- practicas/modal/js/animation-control.js
+++ practicas/modal/js/animation-control.js
@@ -1,1 +1,7 @@
 const $hiddeButton = document.getElementById("hide-modal");
+const $modal = document.getElementById("modal");
+const $overlay = document.getElementById("overlay");
+$hiddeButton.addEventListener('click', (event) => {
+    $modal.style.animation = 'modalOut .8s forwards';
+    $overlay.classList.remove('active');
+});
